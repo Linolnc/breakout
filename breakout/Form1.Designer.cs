4321eba020c50b3aaedce94eb46378d51f26d7e4
Lincoln Baker <lincbake795@ed.amdsb.ca>
2023-05-30 14:53:49 -0400

    Add project files.

---
+++ breakout/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace breakout
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Nameofgame = new System.Windows.Forms.Label();
+            this.EXIT = new System.Windows.Forms.Label();
+            this.DOnt = new System.Windows.Forms.Button();
+            this.Do = new System.Windows.Forms.Button();
+            this.Gaming = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(0, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Nameofgame
+            // 
+            this.Nameofgame.AutoSize = true;
+            this.Nameofgame.Font = new System.Drawing.Font("Microsoft Tai Le", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nameofgame.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Nameofgame.Location = new System.Drawing.Point(254, 63);
+            this.Nameofgame.Name = "Nameofgame";
+            this.Nameofgame.Size = new System.Drawing.Size(140, 31);
+            this.Nameofgame.TabIndex = 1;
+            this.Nameofgame.Text = "BREAKOUT";
+            // 
+            // EXIT
+            // 
+            this.EXIT.AutoSize = true;
+            this.EXIT.Font = new System.Drawing.Font("Microsoft Tai Le", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EXIT.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.EXIT.Location = new System.Drawing.Point(266, 167);
+            this.EXIT.Name = "EXIT";
+            this.EXIT.Size = new System.Drawing.Size(0, 31);
+            this.EXIT.TabIndex = 2;
+            // 
+            // DOnt
+            // 
+            this.DOnt.BackColor = System.Drawing.Color.Black;
+            this.DOnt.Enabled = false;
+            this.DOnt.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.DOnt.FlatAppearance.BorderSize = 0;
+            this.DOnt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DOnt.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DOnt.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.DOnt.Location = new System.Drawing.Point(150, 179);
+            this.DOnt.Name = "DOnt";
+            this.DOnt.Size = new System.Drawing.Size(95, 56);
+            this.DOnt.TabIndex = 3;
+            this.DOnt.Text = "EXIT";
+            this.DOnt.UseVisualStyleBackColor = false;
+            this.DOnt.Click += new System.EventHandler(this.DOnt_Click);
+            // 
+            // Do
+            // 
+            this.Do.BackColor = System.Drawing.Color.Black;
+            this.Do.Enabled = false;
+            this.Do.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Do.FlatAppearance.BorderSize = 0;
+            this.Do.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Do.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Do.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Do.Location = new System.Drawing.Point(393, 179);
+            this.Do.Name = "Do";
+            this.Do.Size = new System.Drawing.Size(95, 56);
+            this.Do.TabIndex = 4;
+            this.Do.Text = "PLAY";
+            this.Do.UseVisualStyleBackColor = false;
+            this.Do.Click += new System.EventHandler(this.Do_Click);
+            // 
+            // Gaming
+            // 
+            this.Gaming.Enabled = true;
+            this.Gaming.Interval = 20;
+            this.Gaming.Tick += new System.EventHandler(this.Gaming_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(673, 365);
+            this.Controls.Add(this.Do);
+            this.Controls.Add(this.DOnt);
+            this.Controls.Add(this.EXIT);
+            this.Controls.Add(this.Nameofgame);
+            this.Controls.Add(this.textBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint_1);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Nameofgame;
+        private System.Windows.Forms.Label EXIT;
+        private System.Windows.Forms.Button DOnt;
+        private System.Windows.Forms.Button Do;
+        private System.Windows.Forms.Timer Gaming;
+    }
+}
+
---
+++ breakout/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace breakout
+{
+    public partial class Form1 : Form
+    {
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        Rectangle player = new Rectangle(500, 300, 100, 10);
+        Rectangle Ball = new Rectangle(480, 30, 10, 10);
+        int playerspeed = 4;
+        int ballspeed = 6; 
+
+
+        //keys
+        bool leftArrowDown = false;
+        bool rightArrowDown = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DOnt_Click(object sender, EventArgs e)
+        {
+            //just to close the tab
+            Application.Exit();
+        }
+
+        private void Do_Click(object sender, EventArgs e)
+        {
+
+            Gaming.Start();
+            this.Focus();
+            Nameofgame.Visible = false;
+            Do.Visible = false;
+            Do.Enabled = false;
+            DOnt.Enabled = false;
+            DOnt.Visible = false;
+
+            Do.Dispose();
+            DOnt.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void Gaming_Tick(object sender, EventArgs e)
+        {
+            //movement
+            if (leftArrowDown == true && player.X > 0)
+            {
+                player.X -= playerspeed;
+            }
+
+            if (rightArrowDown == true && player.X < this.Width - player.Width)
+            {
+                player.X += playerspeed;
+            }
+
+            Refresh();
+
+        }
+
+        private void Form1_Paint_1(object sender, PaintEventArgs e)
+        {
+            if (Gaming.Enabled == true)
+            {
+                e.Graphics.FillRectangle(whiteBrush, player);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftArrowDown = false;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = false;
+                    break;
+            }
+        }
+
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = true;
+                    break;
+            }
+        }
+    }
+}
